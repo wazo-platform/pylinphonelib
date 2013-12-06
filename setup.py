265da1b24331a4e4f8a8f8ead85701ff6ede3460
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2013-12-06 15:30:11 -0500

    put some stuff in setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+pylinphonelib
+-------------
+
+pylinphonelib is a library to drive a linphonec instance from python
+"""
+
+from setuptools import setup
+
+setup(
+    name='pylinphonelib',
+    version='0.0',
+    licence='BSD',
+    author='Pascal Cadotte Michaud',
+    author_email='pascal.cadotte@gmail.com',
+    description='A library to drive linphonec',
+    long_description=__doc__,
+    packages=['linphonelib'],
+    platforms='any',
+)
