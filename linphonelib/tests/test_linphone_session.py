1ed912ea1199face002a69c2b9d4a3bed1774e95
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-03-08 11:38:01 -0500

    fix unittests

--- linphonelib/tests/test_linphone_session.py
+++ linphonelib/tests/test_linphone_session.py
@@ -96,7 +96,7 @@ class TestShell(TestCase):
 
     @patch('linphonelib.linphonesession.QuitCommand', Mock())
     def test_start(self):
-        launch_command = 'docker run --rm -ti -v {linphonerc}:/root/.linphonerc xivo-linphone'
+        launch_command = 'sh -c "linphonec -c {linphonerc}" &'
         s = _Shell(sentinel.port, sentinel.rtp_port)
         s._create_config_file = Mock(return_value=self._filename)
 
