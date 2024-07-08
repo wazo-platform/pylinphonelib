e9c04af4d4a96edabf573e813e20c00af7872432
Sebastien Duthil <sduthil@wazo.community>
2024-07-08 11:20:42 -0400

    server: improve waiting for server to be up
    Why:
    
    * sometimes waiting returned ready, but socket connection returned
      "Connection refused"

--- linphonelib/server.py
+++ linphonelib/server.py
@@ -1,9 +1,11 @@
-# Copyright 2019-2023 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import os
 import subprocess
 import time
+
+from linphonelib.client import LinphoneClient
 
 
 # NOTE: Improve using docker python library
@@ -39,7 +41,7 @@ class LinphoneServer:
         ]
         self._log_write('Starting linphone container...')
         subprocess.run(cmd, stdout=subprocess.DEVNULL)
-        self._log_write('Waiting linphone container is ready...')
+        self._log_write('Waiting for linphone container to be ready...')
         self._wait_until_ready()
         self._log_write('Linphone container ready!')
 
@@ -47,14 +49,12 @@ class LinphoneServer:
         cmd = ['docker', 'kill', self._docker_name]
         subprocess.run(cmd)
 
-    def _is_ready(self):
-        return os.path.exists(self._socket_file)
-
     def _wait_until_ready(self):
+        client = LinphoneClient(self._socket_file, self._logfile)
         tries = 10
         interval = 0.5
         for _ in range(tries):
-            if self._is_ready():
+            if client.is_server_up():
                 return
             time.sleep(interval)
 
