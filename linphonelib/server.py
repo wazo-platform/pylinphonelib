33ef25b40347c760ab65cce577539d3ee7463883
Sebastien Duthil <sduthil@wazo.community>
2024-10-25 14:44:01 -0400

    logs: dump server logs after closing server

--- linphonelib/server.py
+++ linphonelib/server.py
@@ -1,11 +1,17 @@
 # Copyright 2019-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from __future__ import annotations
+
 import os
 import subprocess
 import time
+from typing import TYPE_CHECKING, TextIO
 
 from linphonelib.client import LinphoneClient
+
+if TYPE_CHECKING:
+    from tempfile import _TemporaryFileWrapper
 
 
 # NOTE: Improve using docker python library
@@ -31,7 +37,6 @@ class LinphoneServer:
         cmd = [
             'docker',
             'run',
-            '--rm',
             '--detach',
             '--name',
             self._docker_name,
@@ -40,14 +45,31 @@ class LinphoneServer:
             self._DOCKER_IMG,
         ]
         self._log_write('Starting linphone container...')
-        subprocess.run(cmd, stdout=subprocess.DEVNULL)
+        completed_process = subprocess.run(cmd, stdout=subprocess.PIPE)
+        self._container_id = completed_process.stdout.decode('utf-8').strip()
         self._log_write('Waiting for linphone container to be ready...')
         self._wait_until_ready()
         self._log_write('Linphone container ready!')
 
+    def dump_container_output(self, log_file: TextIO | _TemporaryFileWrapper):
+        self._log_write('Linphone server logs:')
+        completed_process = subprocess.run(
+            ['docker', 'logs', '--timestamps', self._container_id],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+        log_file.write(completed_process.stdout.decode('utf-8'))
+        self._log_write('End of Linphone server logs')
+
     def force_stop(self):
         cmd = ['docker', 'kill', self._docker_name]
         subprocess.run(cmd)
+
+    def cleanup(self):
+        subprocess.run(
+            ['docker', 'rm', self._container_id],
+            stdout=subprocess.DEVNULL,
+        )
 
     def _wait_until_ready(self):
         client = LinphoneClient(self._socket_file, self._logfile)
